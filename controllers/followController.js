09b6352d00b6b7ada30e0b7f2cecbc1573888747
Hardik Beladiya <beladiyahardik755@gmail.com>
2022-02-14 15:58:11 +0530

    Follow and following

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,11 +1,14 @@
 const User = require('../schema/userSchema')
 
-const followUser = async (req,res) => {
-    await User.updateOne({username: req.body.followto},{$push: {followers:[req.body.username]}}).then((result) => {
-        res.send(`Now ${req.body.username} following ${req.body.followto}`)
+const followUser = async (req, res) => {
+    await User.updateOne({ username: req.body.followto }, { $push: { followers: [req.body.username] } }).then((result) => {
+        User.updateOne({ username: req.body.username }, { $push: { following: [req.body.followto] } }).then(() => {
+            res.send(`Now ${req.body.username} following ${req.body.followto}`)
+        })
     }).catch((err) => {
         res.send('Failed')
     })
+
 }
 
 module.exports = {
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,34 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    index: { unique: true },
-  },
-  fullname: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  birthday: {
-    type: String,
-    required: true,
-  },
-  doj: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-  followers: {
-    type: Array
-  },
-  following: {
-    type: Array
-  }
+  username: { type: String, required: true, index: { unique: true } },
+  fullname: { type: String, required: true },
+  password: { type: String, required: true },
+  birthday: { type: String, required: true },
+  doj: { type: Date, required: true, default: Date.now },
+  followers: { type: Array },
+  following: { type: Array }
 });
 
 module.exports = mongoose.model("User", userSchema);
